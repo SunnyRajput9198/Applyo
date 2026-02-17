db09181b9a8355afd6858870859f1c2d58b9e079
sunny <rajputsny2@gmail.com>
2026-02-17 21:20:27 +0530

    changsd ui

--- applyo/apps/frontend/tailwind.config.js
+++ applyo/apps/frontend/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./apps/frontend/index.html",
+    "./apps/frontend/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
